561767aea2c603a2b6883fbca71090c725c8f0bf
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-02-01 17:35:23 +0600

    Homework 01 done

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Message from "./Message";
 
 const messageData = {
-    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Yana',
-    message: 'How r u!?',
+    avatar: 'https://avatarfiles.alphacoders.com/693/69306.jpg',
+    name: 'Homer Simpson',
+    message: "D'Oh!",
     time: '9:00',
 }
 
@@ -14,9 +14,6 @@ function HW1() {
         <div>
             <hr/>
             homeworks 1
-
-            should work (должно работать)
-
             <Message
                 avatar={messageData.avatar}
                 name={messageData.name}
