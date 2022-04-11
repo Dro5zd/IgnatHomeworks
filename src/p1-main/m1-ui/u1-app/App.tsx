d3439b6cdda3f1be6d7e1ccd034fcc2372edcee1
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-04-12 00:32:33 +0600

    Homework 04 test

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import s from './App.module.css'
 import HW5 from '../../../p2-homeworks/h5/HW5';
-
+//test
 function App() {
     return (
         <div className={s.App}>
