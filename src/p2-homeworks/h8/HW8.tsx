9068396dbceea8d2d9c47b5e9f182aa5208daf0d
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-04-15 21:01:14 +0600

    Homework 08 finish1

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import {homeWorkReducer} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './H8.module.css';
 
 export type UserType = {
     _id: number,
@@ -21,7 +22,7 @@ function HW8() {
     const [people, setPeople] = useState<UserType[]>(initialPeople)
 
     const finalPeople = people.map((p: UserType) => (
-        <div key={p._id}>
+        <div key={p._id} className={s.title}>
             {p.name}
             {p.age}
         </div>
@@ -37,12 +38,15 @@ function HW8() {
         <div>
             <hr/>
             homeworks 8
+            <div className={s.container}>
+                {finalPeople}
+                <div className={s.btn_block}>
+                    <div><SuperButton onClick={sortUp} className={s.btn8}>sort up</SuperButton></div>
+                    <div><SuperButton onClick={sortDown} className={s.btn8}>sort down</SuperButton></div>
+                    <div><SuperButton onClick={check18} className={s.btn8}>check 18</SuperButton></div>
+                </div>
+            </div>
 
-            {finalPeople}
-
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div><SuperButton onClick={sortDown}>sort down</SuperButton></div>
-            <div><SuperButton onClick={check18}>check 18</SuperButton></div>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
