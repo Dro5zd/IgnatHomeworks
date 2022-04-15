da698bc3650e493ae48b4c1d8e2afa26738ad1fc
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-04-15 20:19:04 +0600

    Homework 08 finish

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,12 +1,28 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from '../HW8';
+
+type SortType = {
+    type: 'sort',
+    payload: string
+}
+
+type CheckType = {
+    type: 'check',
+    payload: number
+}
+
+export type ActionType = SortType | CheckType
+
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
-        }
+            const newState = [...state].sort((a, b) =>
+                (a.name > b.name) ? 1 : (b.name > a.name) ? -1 : 0)
+
+            return action.payload === 'up' ? newState : newState.reverse()
+    }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(f => f.age > action.payload)
         }
         default: return state
     }
