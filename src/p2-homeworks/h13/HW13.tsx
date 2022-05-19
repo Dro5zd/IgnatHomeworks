5a6589abdd458f2fa1cab1edd55efa4e0f74cf7f
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-05-19 18:14:56 +0600

    Homework 13 start

---
+++ src/p2-homeworks/h13/HW13.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './HW13.module.css';
+import {Request} from './Requests';
+
+
+export const HW13 = () => {
+
+    return (
+        <div className={s.container}>
+            <Request/>
+        </div>
+
+    );
+}
+
---
+++ src/p2-homeworks/h13/Requests.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Request = () => {
+    return (
+        <div>
+            <button></button>
+            <input type="checkbox"/>
+        </div>
+    );
+};
