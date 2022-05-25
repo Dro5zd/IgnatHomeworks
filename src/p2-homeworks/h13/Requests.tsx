642b4de843ee98d861b4666ceb22fe7e305c8a8a
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-05-25 16:50:59 +0600

    Homework 13 done final

--- src/p2-homeworks/h13/Requests.tsx
+++ src/p2-homeworks/h13/Requests.tsx
@@ -10,21 +10,28 @@ export const Request = memo(() => {
     const checkedHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setIsChecked(e.currentTarget.checked)
     }
-    const sendHandler = useCallback( () => {
+    const sendHandler = useCallback(() => {
         requestAPI.sendRequest(isChecked)
-            .then((res) => {setError(res.data.errorText)})
+            .then((res) => {
+                setError(res.data.errorText)
+            })
             .catch((error) => {
-                // console.log({...error});
-                // console.log(error.response ? error.response.data.errorText : error.message);
+                console.log({...error});
+                console.log(error.response ? error.response.data.errorText : error.message);
                 setError(error.response ? error.response.data.errorText : error.message);
             })
     }, [isChecked])
 
+
     return (
-        <div className={s.loadingContainer}>
-            <div><button onClick={sendHandler}>Send</button></div>
-            <div> <input type="checkbox" onChange={checkedHandler}/></div>
-            <div><span >{error}</span></div>
+        <div className={s.mainContainer}>
+            <div>
+                <button className={s.btn13} onClick={sendHandler}>Send</button>
+            </div>
+            <div>
+                <input type="checkbox" onChange={checkedHandler}/>
+            </div>
+            <span>{error}</span>
         </div>
     );
 });
